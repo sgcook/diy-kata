59d7072a08b93ef8bf1b7666522567e330080457
Sara Cook <saracook087@gmail.com>
2022-03-21 20:18:43 +0000

    solved kata7

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,8 @@
 const getEmployerRole = (employeeName, employees) => {
-    
+  const jobs = Object.values(employees)
+    .filter(i => i.name === employeeName)
+    .map(x => x.role);
+  return jobs.toString();
 };
 
 module.exports = getEmployerRole;
